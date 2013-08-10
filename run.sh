987eea53f3d75fc4fbb52593d267e93e9170e3a0
notogawa <n.ohkawa@gmail.com>
2013-08-11 08:04:30 +0900

    ignore status

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ set -e
 touch continue
 while test -f continue
 do
-  BV-infer/dist/build/BV-infer/BV-infer +RTS -N12 -M30G -RTS
+  BV-infer/dist/build/BV-infer/BV-infer +RTS -N12 -M30G -RTS || true
   echo
 done
